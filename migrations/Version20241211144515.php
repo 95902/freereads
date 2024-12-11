fecede8ecb6eba809a4608197ee69e5cadac3a9b
95902 <Bragance.m@hotmail.fr>
2024-12-11 16:32:41 +0100

    📦 NEW:  intégration l'API Google Books

---
+++ migrations/Version20241211144515.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241211144515 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book ALTER small_thumbail TYPE TEXT');
+        $this->addSql('ALTER TABLE book ALTER thumbail TYPE TEXT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE book ALTER small_thumbail TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE book ALTER thumbail TYPE VARCHAR(255)');
+    }
+}
